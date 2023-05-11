42109e21dadcdb24473a0fe6a253fa7113d39271
Norwid Behrnd <nbehrnd@yahoo.com>
2023-05-11 22:28:05 +0200

    shorten the moderator script for svgcleaner
    The reorganization aims to provide a basis easier to maintain.

--- dek_optimize_5.sh
+++ dek_optimize_5.sh
@@ -4,93 +4,91 @@
 # author:  nbehrnd@yahoo.com
 # license: MIT, 2020
 # date:    2020-05-31 (YYYY-MM-DD)
-# edit:    2020-09-09 (YYYY-MM-DD)
+# edit:    [2023-05-11 Thu]
 #
 #
-# The DEK .svg fetched from Wikimedia contain much more information,
-# than building an Anki deck requires.  Removing e.g. commentary lines
-# and simplifying the paths to draw the illustrations thus reduces the
-# individual file size, and globally the Anki deck (20k+ entries).
+# For the display of .svg in an Anki deck about DEK, the files carry
+# more details of information, than necessary.  Because the slides
+# in the Anki deck are not intended to be processed further, it is
+# assumed safe to remove many of the (meta) data, to trim the levels
+# of indentation, reduce the precision of coordinates of Bezier 
+# curves and vector strokes.  For this, this bash script moderates
+# the action of svgcleaner.[1]  In comparison to the initial .svg,
+# the newly written files are typically are 25 to 20% of the original
+# file size which is significant for a corpus of close to 40k files.
 #
-# This bash script moderates the Linux executable of svgcleaner,[1]
-# included as a copy in this project, too.  Deposit both this bash
-# script dek_optimize_5.sh and svgcleaner as copies in the folder one
-# level above folder "raw_data" (present since working for the first
-# time with the .svg) and "antechamber" (visible while preparing an
-# update of the Anki deck).  Add the executable bit and run the script
-# from the CLI by
+# Deposit both this bash script dek_optimize_5.sh and svgcleaner as
+# copies in the folder containing the "raw_data".  After provision of
+# the executable bit to both run
 #
 # ./dek_optimize_5.sh
 #
-# Depending on the explicit choice (initial work / updating work), the
-# location where the script simplifies the .svg is adjusted.  Often,
-# the apparent size is about 20...25% of the originally submitted .svg.
-#
 # At any time, a "Ctrl + C" command will stop the script's action.
 #
-# [1] https://github.com/RazrFalcon/svgcleaner, v0.9.5 / Apr 10, 2018."""
+# [1] https://github.com/RazrFalcon/svgcleaner in version 0.9.5 by
+#     Apr 10, 2018."""
 
 # Explicitly choose between initial work, or update.
 #
 # The case-wise design is inspired by an answer at
 # https://stackoverflow.com/questions/5542016/bash-user-input-if
-read -n1 -p "Choose between [1] initial work, [2] update, or [q]uit. " doit
+read -n1 -p "Choose between [1] initial work, or [q]uit. " doit
 case $doit in
     1) choice=10 ;;
-    2) choice=20 ;;
     q) choice=0 ;;
 esac
 
 
 # Feedback to the user:
-if [ $choice = 10 ]
+if [ "$choice" = 10 ]
 then
-    printf "\n[1]    Processing of initial data copied from 'raw_data'.\n";
-elif [ $choice = 20 ]
+    printf "\n[1] Process the original data by overwrite";
+elif [ "$choice" = 0 ]
 then
-    printf "\n[2]    Processing of update data copied from 'antechamber'.\n";
-elif [ $choice = 0 ]
-then
-    printf "\n[q]    Closing of the script.\n";
+    printf "\n[q] Close the script.\n";
 fi
 
 # Simplification of the .svg:
-if [ $choice = 10 ]
+if [ "$choice" = 10 ]
 then
-    cp ./svgcleaner ./raw_data/dek_workshop;
-    cd ./raw_data/dek_workshop;
+    mkdir prior_optimization;
+    cp ./*.svg ./prior_optimization;
     chmod u+x ./svgcleaner;
-elif [ $choice = 20 ]
+elif [ "$choice" = 0 ]
 then
-    cp ./svgcleaner ./antechamber/dek_workshop;
-    cd ./antechamber/dek_workshop;
-    chmod u+x ./svgcleaner;
+    exit
 fi
 
 for num in {1..5}
 do
-    
+
     for file in *.svg; do
         echo ""
         echo "cycle $num of 5, processing $input_file"  # CLI reporter
 
-        input_file=$file
+        input_file="$file"
         extent_intermediate='_inter.svg'
         intermediate_file=$input_file$extent_intermediate
 
-        ./svgcleaner $input_file $intermediate_file --indent 1 --remove-comments --remove-unused-defs --convert-shapes --remove-metadata --remove-nonsvg-attributes --remove-unreferenced-ids --remove-text-attributes --trim-colors --simplify-transforms --paths-coordinates-precision 1
-        mv $intermediate_file $input_file
+        ./svgcleaner "$input_file" "$intermediate_file" --indent 1 \
+            --remove-comments --remove-unused-defs --convert-shapes \
+            --remove-metadata --remove-nonsvg-attributes \
+            --remove-unreferenced-ids --remove-text-attributes \
+            --trim-colors --simplify-transforms \
+            --paths-coordinates-precision 1
+        mv "$intermediate_file" "$input_file"
     done
 
 done
 
 # Finalize the work:
-rm svgcleaner  # space cleaning
+# rm svgcleaner  # space cleaning
 
 echo ""
-echo "Number of .svg files in folder 'dek_workshop':"
-ls *.svg | wc -l
-echo "Number of all files in folder 'dek_workshop':"
-ls *.* | wc -l
-echo "Apparent file size of all files in folder 'dek_workshop':"
-du -h --apparent-size
+echo "----"
+printf "Number of .svg prior optimization:"; \
+    find ./prior_optimization/*.svg | wc -l
+printf "Number of .svg after optimization:"; \
+    find ./*.svg | wc -l
+
+# EOF
