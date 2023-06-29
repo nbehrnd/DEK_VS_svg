cdbfccf84fa42501ea44032bd0ada9bb9d8d16e9
Norwid Behrnd <nbehrnd@yahoo.com>
2023-06-29 15:56:41 +0200

    sort optimization paramaters
    The sequence of optimization parameters the moderated linter
    `svgcleaner` uses now follows the sequence in the documentation.
    
    Signed-off-by: Norwid Behrnd <nbehrnd@yahoo.com>

--- dek_optimize_5c.sh
+++ dek_optimize_5c.sh
@@ -4,7 +4,7 @@
 # author:  nbehrnd@yahoo.com
 # license: MIT, 2023
 # date:    <2023-06-13 Wed>
-# edit:
+# edit:    <2023-06-29 Thu>
 #
 # Concept study to moderate svgcleaner.  Deposit both this bash script
 # and the svgcleaner (with provision of the executable bit), and run
@@ -15,23 +15,35 @@
 # for a comparison with sibling `dek_optimize_5b.sh`.  Instead of piping
 # an intermediate file (stream) across bash anew into the svgcleaner, the
 # discovered `--multipass`, the svgcleaner possibly processes the .svg
-# until changes of the file size underpass a critical threshold.  Because
-# there is little explicit documentation about this option on the project's
-# (now archived) GitHub page, this is a speculation.
-# In comparison to `dek_optimize_5b.sh` and a set of 3936 DEK .svg, now 
-# all files submitted yield a usable file (script 5b yielded 22 empty files,
-# a failure rate of about 0.6%).  Second, the reduction of file size of the
-# set is very much comparable with the one obtained with script 5b with the
-# additional benefit of a overall processing time of only about 60% of the
-# one with script 5b.
+# until changes of the file size underpass a critical threshold.  Command
+# options selected are in the sequence of the now archived documentation of
+# the project, see
+#
+# https://github.com/RazrFalcon/svgcleaner
+# https://github.com/RazrFalcon/svgcleaner/blob/master/docs/svgcleaner.adoc
+#
+# for additional background and typical processing of motifs.
 
 # parameters applicable:
-parameters="--quiet --indent 1 \
-    --remove-comments --remove-unused-defs --convert-shapes \
-    --remove-metadata --remove-nonsvg-attributes \
-    --remove-unreferenced-ids --remove-text-attributes \
-    --trim-colors --simplify-transforms \
-    --paths-coordinates-precision 1 --multipass"
+parameters="--quiet --indent none \
+  --remove-comments --remove-declarations --remove-nonsvg-elements \
+  --remove-unused-defs --convert-shapes --remove-title --remove-desc \
+  --remove-metadata --remove-nonsvg-attributes --remove-dupl-lineargradient \
+  --remove-dupl-radialgradient --remove-dupl-fegaussianblur --ungroup-groups \
+  --ungroup-defs --merge-gradients --regroup-gradient-stops \
+  --remove-invisible-elements --resolve-use --remove-version \
+  --remove-unreferenced-ids --trim-ids \
+  --remove-text-attributes --remove-unused-coordinates \
+  --remove-default-attributes --remove-xmlns-xlink-attribute \
+  --remove-needless-attributes --remove-gradient-attributes \
+  --apply-transform-to-gradients --apply-transform-to-shapes \
+  --remove-unresolved-classes --paths-to-relative --remove-unused-segments\
+  --convert-segments --apply-transform-to-paths --trim-paths \
+  --join-arcto-flags --remove-dupl-cmd-in-paths --use-implicit-cmds \
+  --trim-colors --simplify-transforms \
+  --coordinates-precision 1 ----properties-precision 1 \
+  --paths-coordinates-precision 1 --list-separator space \
+  --multipass"
 
 
 for file in *.svg
